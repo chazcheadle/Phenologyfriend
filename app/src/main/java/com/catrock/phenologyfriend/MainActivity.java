f2bccbf9fd54ed21cb5e90c093784ee13d30cedb
Chaz Cheadle <charles.cheadle@nbcuni.com>
2015-05-08 16:40:08 -0400

    Update string names. Remove support.v7 support library. Update theme

--- app/src/main/java/com/catrock/phenologyfriend/MainActivity.java
+++ app/src/main/java/com/catrock/phenologyfriend/MainActivity.java
@@ -2,13 +2,14 @@ package com.catrock.phenologyfriend;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return(true);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
