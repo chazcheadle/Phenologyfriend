fd09570dc1be7cb44cea714a9087fb6aa7df7c73
Chaz Cheadle <charles.cheadle@nbcuni.com>
2015-05-29 00:48:22 -0400

    WIP: Adding specimen autocomplete

--- app/src/main/java/com/catrock/phenologyfriend/AddObservationPlantActivity.java
+++ app/src/main/java/com/catrock/phenologyfriend/AddObservationPlantActivity.java
@@ -4,12 +4,24 @@ import android.app.Activity;
 import android.content.ContentValues;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 
 public class AddObservationPlantActivity extends Activity {
+
+    CustomAutoCompleteView plantAutocomplete;
+
+    ArrayAdapter<SpecimenData> myPlantAutocompleteAdapter;
+
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,45 @@ public class AddObservationPlantActivity extends Activity {
         WeatherData weatherData= getIntent().getParcelableExtra("/weatherDataTag");
         if (weatherData != null) {
             Toast.makeText(this, weatherData.getTemperature(), Toast.LENGTH_LONG).show();
+        }
+        try{
+
+            // instantiate database handler
+           // db = new DatabaseHelper;
+
+            // put sample data to database
+          //  insertSampleData();
+
+            // autocompletetextview is in activity_main.xml
+            plantAutocomplete = (CustomAutoCompleteView) findViewById(R.id.addPlantAutocomplete);
+
+            plantAutocomplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View arg1, int pos, long id) {
+
+                    RelativeLayout rl = (RelativeLayout) arg1;
+                    TextView tv = (TextView) rl.getChildAt(0);
+                    plantAutocomplete.setText(tv.getText().toString());
+
+                }
+
+            });
+
+            // add the listener so it will tries to suggest while the user types
+            plantAutocomplete.addTextChangedListener(new CustomAutoCompleteTextChangedListener(this));
+
+            // ObjectItemData has no value at first
+            SpecimenData[] specimenData = new SpecimenData[0];
+
+            // set the custom ArrayAdapter
+            myPlantAutocompleteAdapter = new Adapter(this, R.layout.specimen_autocomplete, specimenData);
+            plantAutocomplete.setAdapter(myPlantAutocompleteAdapter);
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
